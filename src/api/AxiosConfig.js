70f3cf7ee890b8c6505663fb0ebc9dbc23bd905d
Yashd07 <yashd1272@gmail.com>
2023-12-10 00:22:03 +0530

    initial commit

---
+++ src/api/AxiosConfig.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'http://localhost:8080',
+    headers: {"ngrok-skip-browser-warning": "true"}
+})
+
+//https://d414-202-148-60-46.ngrok-free.app
